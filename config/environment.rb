5f9d984fd808f47245adf7039bfde333b3800343
Derrick Carr <derrick.the.future@gmail.com>
2017-02-14 23:44:16 -0800

    Require awesome_print gem

--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,7 @@ require 'haml'
 require 'httparty'
 
 require 'json'
+require 'awesome_print'
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
